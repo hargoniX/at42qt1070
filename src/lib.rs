59b96cf97932281b82bfbd4f350b3874a577c773
Henrik Böving <hargonix@gmail.com>
2020-04-25 16:34:23 +0200

    get_status and calibrate function with bitfield crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
-use embedded_hal::blocking::i2c::WriteRead;
+use embedded_hal::blocking::i2c::{Write, WriteRead};
+use bitfield::bitfield;
 
 pub enum Error<I2cError> {
     I2cError(I2cError),
@@ -12,13 +13,21 @@ impl<E> From<E> for Error<E> {
     }
 }
 
+bitfield!{
+    pub struct Status(u8);
+    impl Debug;
+    calibrate, _: 7;
+    overflow, _: 6;
+    touch, _: 0;
+}
+
 pub struct Driver<I2C> {
     i2c: I2C,
 }
 
 impl<I2C, I2cError> Driver<I2C>
 where
-    I2C: WriteRead<Error = I2cError>,
+    I2C: WriteRead<Error = I2cError> + Write<Error = I2cError>,
 {
     pub fn new(i2c: I2C) -> Result<Driver<I2C>, Error<I2cError>> {
         let mut driver = Driver {
@@ -33,6 +42,24 @@ where
         Ok(driver)
     }
 
+    pub fn calibrate(&mut self) -> Result<(), Error<I2cError>> {
+        self.i2c.write(Chip::I2C, &[Chip::CALIBRATE_ADDR, 0xFF])?;
+
+        loop {
+            let status = self.get_status()?;
+            if !status.calibrate {
+                break;
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn get_status(&mut self) -> Result<Status, Error<I2cError>> {
+        let mut buffer = [0u8; 1];
+        self.i2c.write_read(Chip::I2C, &[Chip::STATUS_ADDR], &mut buffer)?;
+        Ok(Status(buffer[0]))
+    }
     fn get_id(&mut self) -> Result<u8, Error<I2cError>> {
         let mut buffer = [0u8; 1];
         self.i2c.write_read(Chip::I2C, &[Chip::ID_ADDR], &mut buffer)?;
@@ -43,5 +70,7 @@ where
 mod Chip {
     pub const I2C: u8 = 0x1B << 1;
     pub const ID: u8 = 0x2E;
-    pub const ID_ADDR: u8 = 0x0;
+    pub const ID_ADDR: u8 = 0;
+    pub const STATUS_ADDR: u8 = 2;
+    pub const CALIBRATE_ADDR: u8 = 56;
 }
